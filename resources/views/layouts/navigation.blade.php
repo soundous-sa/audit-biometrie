5120e0d49533e4a459372a594be87665e49087ce
soundous <soundous20sassi02@gmail.com>
2025-08-27 21:45:28 +0200

    fix the edit blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,3 @@
-
-@extends('layouts.admin')
-
-@section('content')
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
